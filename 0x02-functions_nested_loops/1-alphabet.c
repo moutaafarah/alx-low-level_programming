63d7c0954934c328d2988e5fb6f84bb2edef0d4b
moutaafarah <moutaafarah@gmail.com>
2023-03-21 03:07:49 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include ""main.h"
+
+/**
+* print_alphabet - prints lowercase alphabet
+* Return: 0
+*/
+
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++
+	}
+	_putchar('\n');
+}
