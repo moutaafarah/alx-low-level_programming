324a1e928430bef6cd198b9c32cf33dd60f73958
moutaafarah <moutaafarah@gmail.com>
2023-04-28 12:10:21 -0700

    task 3

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,14 +11,14 @@
 */
 int _strlen(const char *s)
 {
-   int i = 0;
+	int i = 0;
 
 
-   while (s[i] != '\0')
-   {
-       i++;
-   }
-   return (i);
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 
 
@@ -30,19 +30,19 @@ int _strlen(const char *s)
 */
 list_t *add_node(list_t **head, const char *str)
 {
-   list_t *add;
+	list_t *add;
 
 
-   add = malloc(sizeof(list_t));
-   if (add == NULL)
-       return (NULL);
-   add->str = strdup(str);
+	add = malloc(sizeof(list_t));
+	if (add == NULL)
+	return (NULL);
+	add->str = strdup(str);
 
 
-   add->len = _strlen(str);
-   add->next = *head;
-   *head = add;
+	add->len = _strlen(str);
+	add->next = *head;
+	*head = add;
 
 
-   return (add);
+	return (add);
 }
