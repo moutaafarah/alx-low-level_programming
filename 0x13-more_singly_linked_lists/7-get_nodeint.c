dbbd3ce6617c817f0e16ddf833f5f8aa968a34f7
moutaafarah <moutaafarah@gmail.com>
2023-05-02 11:52:18 -0700

    fix  one to go

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,8 +11,9 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int a;
-	
+
 	for (a = 0; a < index && head != NULL; a++)
 		head = head->next;
 
 	return (head);
+}
